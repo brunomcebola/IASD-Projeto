252b8c6c01138d0258769646573aa6812be6f207
Bruno Cebola <bruno.m.cebola@gmail.com>
2021-10-03 16:17:50 +0100

    find_closest_points function working

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
 from registration import registration
 from get_pointcloud import point_cloud_data
+from math import sqrt, inf
 
 import numpy as np
 from typing import Tuple
 
 
 class registration_iasd(registration):
-
     def __init__(self, scan_1: np.array((..., 3)), scan_2: np.array((..., 3))) -> None:
 
         # inherit all the methods and properties from registration
@@ -14,7 +14,7 @@ class registration_iasd(registration):
 
         return
 
-    def compute_pose(self,correspondences: dict) -> Tuple[np.array, np.array]:
+    def compute_pose(self, correspondences: dict) -> Tuple[np.array, np.array]:
         """compute the transformation that aligns two
         scans given a set of correspondences
 
@@ -23,9 +23,8 @@ class registration_iasd(registration):
         :return: rotation and translation that align the correspondences
         :rtype: Tuple[np.array, np.array]
         """
-        
-        pass
 
+        pass
 
     def find_closest_points(self) -> dict:
         """Computes the closest points in the two scans.
@@ -41,25 +40,38 @@ class registration_iasd(registration):
         :rtype: dict
         """
 
-        pass
+        correspondencies = {}
+
+        for s1_p in range(self.scan_1.shape[0]):
+            correspondencies[str(s1_p)] = {}
+            correspondencies[str(s1_p)]["point_in_pc_1"] = self.scan_1[s1_p]
+            correspondencies[str(s1_p)]["dist2"] = inf
+
+            for s2_p in range(self.scan_2.shape[0]):
+                dist = sqrt(
+                    (self.scan_1[s1_p, 0] - self.scan_2[s2_p, 0]) ** 2
+                    + (self.scan_1[s1_p, 1] - self.scan_2[s2_p, 1]) ** 2
+                    + (self.scan_1[s1_p, 2] - self.scan_2[s2_p, 2]) ** 2
+                )
+
+                if dist < correspondencies[str(s1_p)]["dist2"]:
+                    correspondencies[str(s1_p)]["dist2"] = dist
+                    correspondencies[str(s1_p)]["point_in_pc_2"] = self.scan_2[s2_p]
+
+        return correspondencies
 
 
 class point_cloud_data_iasd(point_cloud_data):
-
     def __init__(
-            self,
-            fileName: str,
-            ) -> None:
-            
+        self,
+        fileName: str,
+    ) -> None:
+
         super().__init__(fileName)
 
         return
 
-
-    def load_point_cloud(
-            self,
-            file: str
-            ) -> bool:
+    def load_point_cloud(self, file: str) -> bool:
         """Loads a point cloud from a ply file
 
         :param file: source file
@@ -73,35 +85,37 @@ class point_cloud_data_iasd(point_cloud_data):
             point_order = []
             while True:
                 line = fp.readline().split()
-            
-                if line[0]=="element":
-                    if line[1]=="vertex":
+
+                if line[0] == "element":
+                    if line[1] == "vertex":
                         n_vertex = int(line[2])
-                elif line[0] == "property" and line[1]=="float":
+                elif line[0] == "property" and line[1] == "float":
                     if line[2] == "x":
                         point_order.append("x")
                     if line[2] == "y":
                         point_order.append("y")
                     if line[2] == "z":
                         point_order.append("z")
-                        
+
                 if line[0] == "end_header":
                     break
-            
-            if len(point_order) != 3 :
+
+            if len(point_order) != 3:
                 return False
 
             for i in range(n_vertex):
                 line = fp.readline().split()
-                
+
                 # point_list = ["","",""]
                 # #[x, z, y]
                 # point_list[0] = line[ point_order.index("x") ]
                 # point_list[1] = line[point_order.index("y")]
                 # point_list[2] = line[point_order.index("z")]
 
-                point_list = [ float(line[point_order.index(j)]) for j in ["x", "y", "z"] ]
-                
+                point_list = [
+                    float(line[point_order.index(j)]) for j in ["x", "y", "z"]
+                ]
+
                 self.data[str(i)] = np.array(point_list)
 
         return True
